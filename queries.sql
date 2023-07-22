0bf1ab4dfe00e141fc8cd9bac90bca59d6724c1e
preranaMH <137148181+preranaMH@users.noreply.github.com>
2023-07-23 01:18:05 +0530

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,9 +1,9 @@
-
 create table movies(mov_id int(2) primary key, title varchar(20),rel_year int(4), dir_id int(3));
 Insert into movies values (1,"tammudu",2000,1);
 Insert into movies values (2,"simha",2012,2);
 Insert into movies values (3,"yamadonga",2009,3);
 Insert into movies values (4,"bahubali",2000,3);
+Insert into movies values (6,"bahu",2000,6);
 Insert into movies values (5,"ps2",2023,4);
 select * from movies;
 create table directors(dir_name varchar(20),dir_id int(2) primary key);
@@ -27,18 +27,14 @@ insert into movie_genre values(5,1);
 select * from movie_genre;
 
 /* queries*/
-select movies.title,directors.dir_name from movies, directors where movies.dir_id=directors.dir_id;
+select movies.title,directors.dir_name from movies join directors on movies.dir_id=directors.dir_id;
 
-select movies.title,movies.rel_year,directors.dir_name from movies, directors where movies.dir_id=directors.dir_id;
+select movies.title,movies.rel_year,directors.dir_name from movies  join directors on movies.dir_id=directors.dir_id;
 
-select directors.dir_name, movies.title from movies, directors where movies.dir_id=directors.dir_id;
+select directors.dir_name, movies.title from movies left join directors on movies.dir_id=directors.dir_id;
 
-select movies.title,movies.rel_year,directors.dir_name from movies, directors where movies.dir_id=directors.dir_id;
+select movies.title,movies.rel_year,directors.dir_name from movies right join directors on movies.dir_id=directors.dir_id;
 
 select movies.title, genres.genre_name from movies, genres,movie_genre where movies.mov_id=movie_genre.mov_id and movie_genre.genre_id=genres.genre_id;
-
-
-
-
 
 
